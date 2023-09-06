680510075935d73b9faa4432b2d34245b34c3963
Ganesh Reddy <ganeshreddy@Ganeshs-MacBook-Pro.local>
2023-09-06 18:43:13 +0530

    Initial commit, Started nvim journey 😎

---
+++ lua/options.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.clipboard = 'unnamedplus' -- change this to <leader> y just like in .ideavimrc
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.tabstop = 4
+vim.o.expandtab = false
+vim.o.shiftwidth = 4
+
+vim.o.updatetime = 300
+
+vim.o.termguicolors = true
+
+vim.o.mouse = 'a'
